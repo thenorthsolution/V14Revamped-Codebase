ec232ffa2459bd134313c22a7753054c8c6ab4ef
b3ssel <bosbesgaming@gmail.com>
2024-06-05 00:42:38 +0200

    fixed some bugs on registering/comparing commands

--- src/events/ready/01registerCommands.js
+++ src/events/ready/01registerCommands.js
@@ -17,52 +17,29 @@ module.exports = async (client) => {
     const applicationCommands = await getApplicationCommands(client, guildId);
 
     for (const localCommand of localCommands) {
-      const { data } = localCommand;
-
-      const commandName = data.name;
-      const commandDescription = data.description;
-      const commandOptions = data.options;
-
-      const existingCommand = await applicationCommands.cache.find(
-        (cmd) => cmd.name === commandName
-      );
+      const commandName = localCommand.data.name;
+      const existingCommand = await applicationCommands.cache.find((cmd) => cmd.name === commandName);
 
       if (existingCommand) {
         if (localCommand.deleted) {
           await applicationCommands.delete(existingCommand.id);
-          console.log(
-            `Application command ${commandName} has been deleted.`.red
-          );
+          console.log(`Application command ${commandName} has been deleted.`.red);
           continue;
         };
 
-        if (commandComparing(existingCommand, localCommand)) {
-          await applicationCommands.edit(existingCommand.id, {
-            name: commandName,
-            description: commandDescription,
-            options: commandOptions,
-          });
-          console.log(
-            `Application command ${commandName} has been edited.`.yellow
-          );
+        const differentCommand = commandComparing(localCommand.data, existingCommand);
+        if (differentCommand) {
+          await applicationCommands.edit(existingCommand.id, differentCommand);
+          console.log(`Application command ${commandName} has been edited.`.yellow);
         };
       } else {
         if (localCommand.deleted) {
-          console.log(
-            `Application command ${commandName} has been skipped, since property "deleted" is set to "true".`
-              .grey
-          );
+          console.log(`Application command ${commandName} has been skipped, since property "deleted" is set to "true".`.grey);
           continue;
         };
 
-        await applicationCommands.create({
-          name: commandName,
-          description: commandDescription,
-          options: commandOptions,
-        });
-        console.log(
-          `Application command ${commandName} has been registered.`.green
-        );
+        await applicationCommands.create(localCommand.data);
+        console.log(`Application command ${commandName} has been registered.`.green);
       };
     };
   } catch (err) {
--- src/utils/commandComparing.js
+++ src/utils/commandComparing.js
@@ -2,7 +2,7 @@ module.exports = (localCommand, existingCommand) => {
   const commandChoices = (choices = []) => {
     return choices.map((choice) => ({
       name: choice.name,
-      nameLocalizations: (choice.nameLocalizations ?? choice.name_localizations) ?? undefined,
+      nameLocalizations: (choice.nameLocalizations ?? choice.name_localizations) ?? {},
       value: choice.value
     }));
   };
@@ -11,27 +11,28 @@ module.exports = (localCommand, existingCommand) => {
     return options.map((option) => ({
       type: option.type ?? 1,
       name: option.name,
-      nameLocalizations: (option.nameLocalizations ?? option.name_localizations) ?? undefined,
+      nameLocalizations: (option.nameLocalizations ?? option.name_localizations) ?? {},
       description: option.description,
-      descriptionLocalizations: (option.descriptionLocalizations ?? option.description_localizations) ?? undefined,
-      options: commandOptions(option.options) ?? undefined,
-      autocomplete: option.autocomplete ?? undefined,
-      channelTypes: (option.channelTypes ?? option.channel_types) ?? undefined,
-      choices: commandChoices(option.choices) ?? undefined,
-      minValue: (option.minValue ?? option.min_value) ?? undefined,
-      maxValue: (option.maxValue ?? option.max_value) ?? undefined,
-      minLength: (option.minLength ?? option.min_length) ?? undefined,
-      maxLength: (option.maxLength ?? option.max_length) ?? undefined,
+      descriptionLocalizations: (option.descriptionLocalizations ?? option.description_localizations) ?? {},
+      options: commandOptions(option.options) ?? [],
+      autocomplete: option.autocomplete ?? null,
+      channelTypes: (option.channelTypes ?? option.channel_types) ?? null,
+      choices: commandChoices(option.choices) ?? [],
+      minValue: (option.minValue ?? option.min_value) ?? null,
+      maxValue: (option.maxValue ?? option.max_value) ?? null,
+      minLength: (option.minLength ?? option.min_length) ?? null,
+      maxLength: (option.maxLength ?? option.max_length) ?? null,
       required: option.required ?? false
     }));
   };
 
   const commandData = (data) => ({
-    nameLocalizations: (data.nameLocalizations ?? data.name_localizations) ?? undefined,
+    name: data.name,
+    nameLocalizations: (data.nameLocalizations ?? data.name_localizations) ?? {},
     description: data.description,
-    descriptionLocalizations: (data.descriptionLocalizations ?? data.description_localizations) ?? undefined,
-    options: commandOptions(data.options) ?? undefined,
-    defaultMemberPermissions: (data.defaultMemberPermissions ?? data.default_member_permissions) ?? undefined,
+    descriptionLocalizations: (data.descriptionLocalizations ?? data.description_localizations) ?? {},
+    options: commandOptions(data.options) ?? [],
+    defaultMemberPermissions: (data.defaultMemberPermissions ?? data.default_member_permissions) ?? null,
     dmPermission: (data.dmPermission ?? data.dm_permission) ?? true,
     nsfw: data.nsfw ?? false
   });
@@ -40,7 +41,7 @@ module.exports = (localCommand, existingCommand) => {
     JSON.stringify(commandData(localCommand), (_, v) => typeof v === "bigint" ? v.toString() : v)
     !==
     JSON.stringify(commandData(existingCommand), (_, v) => typeof v === "bigint" ? v.toString() : v)
-  ) return true;
+  ) return commandData(localCommand);
 
   return false;
 };
