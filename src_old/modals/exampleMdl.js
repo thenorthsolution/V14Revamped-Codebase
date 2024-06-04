e311d0dc4b2087c097692899abcf8369b31682a9
b3ssel <bosbesgaming@gmail.com>
2024-06-04 02:32:11 +0200

    part 2 of updating codebase to an optimised version

--- src_old/modals/exampleMdl.js
+++
@@ -1,9 +0,0 @@
-const { PermissionFlagsBits, EmbedBuilder } = require("discord.js");
-
-module.exports = {
-  customId: "",
-  userPermissions: [],
-  banPermissions: [],
-
-  run: async (client, interaction) => {},
-};
